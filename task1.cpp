f1618e69b4ec275a8dda5aaecb97e7985b822e62
PentagonCoder <harshu33908@gmail.com>
2025-08-03 00:55:58 +0530

    Create task1.cpp

---
+++ task1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //Input Two number 
+    int n;
+    cin>>n;
+    cout<<"enter first number :";
+    int m;
+    cin>>m;
+    cout <<""
+}
